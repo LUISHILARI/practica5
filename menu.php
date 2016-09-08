13fec75b91e88654d013004c151bd1279ef7f412
LUISHILARI <luiz.jhosep@gmail.com>
2016-09-07 21:57:59 -0400

    subiendo la practica5

---
+++ menu.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>principal</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	
+</head>
+
+<body>
+
+<div id="cabecera">
+	<header><br>
+		<h1><center>PRACTICA 5</center> </h1> <br>
+		<nav>
+		   <ul>
+		   	 <li><a href="http://localhost/practica5/ejercicio1.php">EJERCICIO1</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio2.php">EJERCICIO2</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio3.php">EJERCICIO3</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio4.php">EJERCICIO4</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio5.php">EJERCICIO5</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio6.php">EJERCICIO6</a> </li>
+		   	 <br>
+		   	 	 <br>
+		   	 <li><a href="http://localhost/practica5/ejercicio7.php">EJERCICIO7</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio8.php">EJERCICIO8</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio9.php">EJERCICIO9</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio10.php">EJERCICIO10</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio11.php">EJERCICIO11</a> </li>
+		   	 <li><a href="http://localhost/practica5/ejercicio12.php">EJERCICIO12</a> </li>
+		   </ul> 
+	    </nav>
+	</header>
+</div>
+
+
+
+
+<div id="contenido">
+
+<section>
+<center><img src="imagen/linux2.jpg" width="750px"></center> 
+		<h1>DATOS PERSONALES</h1>
+		<P>JOSE LUIS HILARI AVILE</P>
+		<P>CI 9234111 LP</P>
+		<p>Tel (+591)77585649</p>
+
+</section>
+
+</div>
+
+<div id="pie">
+	<footer>
+		<p>&copy; www.practica5.com 2016</p>
+	</footer>
+
+</div>
+
+</body>
+</html>
